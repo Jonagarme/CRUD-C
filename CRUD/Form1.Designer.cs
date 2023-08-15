7ed1302478f33f8e506342a23d649defd612be01
jonagarme <garciajonathan268@gmail.com>
2023-08-15 13:30:29 -0500

    terminado crud

---
+++ CRUD/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace CRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.txt_apellido = new System.Windows.Forms.TextBox();
+            this.txt_puesto = new System.Windows.Forms.TextBox();
+            this.txt_sueldo = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.EmpleadosInfo = new System.Windows.Forms.DataGridView();
+            this.Sueldo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Puesto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.EmpleadosInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(150, 175);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(298, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Informacion Empleados";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ID:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Nombre:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Apellido:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(456, 104);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Puesto:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(456, 145);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Sueldo:";
+            // 
+            // txt_id
+            // 
+            this.txt_id.Location = new System.Drawing.Point(82, 84);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(228, 20);
+            this.txt_id.TabIndex = 9;
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(82, 131);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(228, 20);
+            this.txt_nombre.TabIndex = 10;
+            // 
+            // txt_apellido
+            // 
+            this.txt_apellido.Location = new System.Drawing.Point(82, 175);
+            this.txt_apellido.Name = "txt_apellido";
+            this.txt_apellido.Size = new System.Drawing.Size(228, 20);
+            this.txt_apellido.TabIndex = 11;
+            // 
+            // txt_puesto
+            // 
+            this.txt_puesto.Location = new System.Drawing.Point(505, 101);
+            this.txt_puesto.Name = "txt_puesto";
+            this.txt_puesto.Size = new System.Drawing.Size(228, 20);
+            this.txt_puesto.TabIndex = 12;
+            // 
+            // txt_sueldo
+            // 
+            this.txt_sueldo.Location = new System.Drawing.Point(505, 145);
+            this.txt_sueldo.Name = "txt_sueldo";
+            this.txt_sueldo.Size = new System.Drawing.Size(228, 20);
+            this.txt_sueldo.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button1.Location = new System.Drawing.Point(111, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Insertar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.SpringGreen;
+            this.button2.Location = new System.Drawing.Point(268, 223);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Actualizar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gold;
+            this.button3.Location = new System.Drawing.Point(439, 223);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Editar";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Salmon;
+            this.button4.Location = new System.Drawing.Point(567, 222);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Eliminar";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(621, 35);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "Conectarse";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // EmpleadosInfo
+            // 
+            this.EmpleadosInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.EmpleadosInfo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Sueldo,
+            this.ID,
+            this.Nombre,
+            this.Apellido,
+            this.Puesto});
+            this.EmpleadosInfo.Location = new System.Drawing.Point(111, 298);
+            this.EmpleadosInfo.Name = "EmpleadosInfo";
+            this.EmpleadosInfo.Size = new System.Drawing.Size(543, 150);
+            this.EmpleadosInfo.TabIndex = 19;
+            this.EmpleadosInfo.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.EmpleadosInfo_CellContentClick);
+            // 
+            // Sueldo
+            // 
+            this.Sueldo.HeaderText = "ID";
+            this.Sueldo.Name = "Sueldo";
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "ID";
+            this.ID.HeaderText = "Nombre";
+            this.ID.Name = "ID";
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Apellido";
+            this.Nombre.Name = "Nombre";
+            // 
+            // Apellido
+            // 
+            this.Apellido.HeaderText = "Puesto";
+            this.Apellido.Name = "Apellido";
+            // 
+            // Puesto
+            // 
+            this.Puesto.HeaderText = "Sueldo";
+            this.Puesto.Name = "Puesto";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 476);
+            this.Controls.Add(this.EmpleadosInfo);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_sueldo);
+            this.Controls.Add(this.txt_puesto);
+            this.Controls.Add(this.txt_apellido);
+            this.Controls.Add(this.txt_nombre);
+            this.Controls.Add(this.txt_id);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Firma ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.EmpleadosInfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.TextBox txt_apellido;
+        private System.Windows.Forms.TextBox txt_puesto;
+        private System.Windows.Forms.TextBox txt_sueldo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridView EmpleadosInfo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sueldo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Apellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Puesto;
+    }
+}
+
---
+++ CRUD/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Drawing;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace CRUD
+{
+    
+    public partial class Form1 : Form
+       
+    {
+        MySqlConnection myCon;
+
+        private void conectar()
+        {
+            try
+            {
+                string server = "localhost";
+                string database = "empresa";
+                string username = "root";
+                string pwd = "0801";
+                string connectionString = "server=" + server + ";" + "database=" + database + ";" + "Uid=" + username + ";" + "password=" + pwd + ";";
+
+                myCon = new MySqlConnection(connectionString);
+                myCon.Open();
+                
+            }
+            catch(Exception error) {
+                MessageBox.Show("ERROR!!!"+ error);
+            }
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            conectar();
+            MessageBox.Show("Conectado!!!");
+            
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conectar();
+        }
+
+       
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+       
+
+    private void EmpleadosInfo_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                llenarTabla();
+                Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al llenar la tabla: " + ex.Message);
+            }
+        }
+
+        private void llenarTabla()
+        {
+            string query = "SELECT numempleado, nombre, apellidopaterno, puesto, sueldo FROM empleados;";
+            MySqlCommand comandoDB = new MySqlCommand(query, myCon);
+            MySqlDataReader reader;
+
+            try
+            {
+                reader = comandoDB.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        int n = EmpleadosInfo.Rows.Add();
+                        EmpleadosInfo.Rows[n].Cells[0].Value = reader.GetString(0);
+                        EmpleadosInfo.Rows[n].Cells[1].Value = reader.GetString(1);
+                        EmpleadosInfo.Rows[n].Cells[2].Value = reader.GetString(2);
+                        EmpleadosInfo.Rows[n].Cells[3].Value = reader.GetString(3);
+                        EmpleadosInfo.Rows[n].Cells[4].Value = reader.GetString(4); 
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No hay empleados");
+                }
+
+                reader.Close(); // Close the reader when done
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "INSERT INTO empleados(numempleado, nombre, apellidopaterno, puesto, sueldo) VALUES (@numempleado, @nombre, @apellidopaterno, @puesto, @sueldo);";
+                MySqlCommand cmd = new MySqlCommand(query, myCon);
+                cmd.Parameters.AddWithValue("@numempleado", txt_id.Text);
+                cmd.Parameters.AddWithValue("@nombre", txt_nombre.Text);
+                cmd.Parameters.AddWithValue("@apellidopaterno", txt_apellido.Text);
+
+                // Convierte el valor del sueldo a decimal
+                decimal sueldo;
+                if (decimal.TryParse(txt_sueldo.Text, out sueldo))
+                {
+                    cmd.Parameters.AddWithValue("@sueldo", sueldo);
+                }
+                else
+                {
+                    MessageBox.Show("El valor del sueldo no es válido.");
+                    return; // Salir del método si el valor del sueldo no es válido
+                }
+
+                cmd.Parameters.AddWithValue("@puesto", txt_puesto.Text);
+
+                myCon.Open();
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Registro Ingresado con Éxito...");
+                    llenarTabla();
+                    EmpleadosInfo.Refresh();
+                }
+                else
+                {
+                    MessageBox.Show("No se Pudo Ingresar el Registro...");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                myCon.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txt_id.Text))
+            {
+                MessageBox.Show("Por favor, ingresa el ID del empleado que deseas eliminar.");
+                return;
+            }
+
+            try
+            {
+                string query = "DELETE FROM empleados WHERE numempleado = @numempleado;";
+                MySqlCommand cmd = new MySqlCommand(query, myCon);
+                cmd.Parameters.AddWithValue("@numempleado", txt_id.Text);
+
+                myCon.Open();
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Registro Eliminado con Éxito...");
+                    llenarTabla();
+                }
+                else
+                {
+                    MessageBox.Show("No se Pudo Eliminar el Registro. Asegúrate de que el ID sea válido.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                myCon.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txt_id.Text))
+            {
+                MessageBox.Show("Por favor, ingresa el ID del empleado que deseas actualizar.");
+                return;
+            }
+
+            try
+            {
+                string query = "UPDATE empleados SET nombre = @nombre, apellidopaterno = @apellidopaterno, sueldo = @sueldo, puesto = @puesto WHERE numempleado = @numempleado;";
+                MySqlCommand cmd = new MySqlCommand(query, myCon);
+                cmd.Parameters.AddWithValue("@numempleado", txt_id.Text);
+                cmd.Parameters.AddWithValue("@nombre", txt_nombre.Text);
+                cmd.Parameters.AddWithValue("@apellidopaterno", txt_apellido.Text);
+
+                // Convierte el valor del sueldo a decimal
+                decimal sueldo;
+                if (decimal.TryParse(txt_sueldo.Text, out sueldo))
+                {
+                    cmd.Parameters.AddWithValue("@sueldo", sueldo);
+                }
+                else
+                {
+                    MessageBox.Show("El valor del sueldo no es válido.");
+                    return;
+                }
+
+                cmd.Parameters.AddWithValue("@puesto", txt_puesto.Text);
+
+                myCon.Open();
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Registro Actualizado con Éxito...");
+                    llenarTabla();
+                }
+                else
+                {
+                    MessageBox.Show("No se Pudo Actualizar el Registro. Asegúrate de que el ID sea válido.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                myCon.Close();
+            }
+        }
+    }
+
+}
